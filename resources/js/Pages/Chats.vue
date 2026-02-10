55f8bbf3d3d098b3e94aa05e24aff8639b10498a
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-10 13:41:12 +0300

    auth chats

--- resources/js/Pages/Chats.vue
+++ resources/js/Pages/Chats.vue
@@ -1,24 +1,26 @@
 <template>
-   <AuthenticatedLayout>
-  <div>
-    <h1>Мои чаты</h1>
-    <ul>
-      <li v-for="chat in chats" :key="chat.id">
-        <a :href="`/chats/${chat.id}`">{{ chat.name }}</a>
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Мои чаты
+      </h2>
+    </template>
 
-      </li>
-    </ul>
-  </div>
-</AuthenticatedLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <ul>
+          <li v-for="chat in chats" :key="chat.id">
+            <a :href="`/chats/${chat.id}`">{{ chat.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
 
 <script setup>
-
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 const props = defineProps({
   chats: Array
-  
 })
-console.log(props.chats)
-
 </script>
