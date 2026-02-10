d60095f06d2df9b55568108f091072d2f79868f0
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-10 14:42:20 +0300

    chat

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Chat; // <-- ВАЖНО!
 
-// app/Http/Controllers/MessageController.php
 class MessageController extends Controller
 {
     public function store(Request $request, Chat $chat)
