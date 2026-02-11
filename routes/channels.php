e230c7661dea9c4a41fd83977aac0131edfaff44
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-11 10:15:16 +0300

    pusher

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
+Broadcast::routes();
+
 Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
     // Проверяем, состоит ли пользователь в этом чате
     return $user->chats()->where('id', $chatId)->exists();
