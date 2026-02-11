b2353742ed4bd75d55a8ddcdc10156f3f937f3bc
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-11 10:05:06 +0300

    pusher

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -105,7 +105,7 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-
+console.log('KEY:', import.meta.env.VITE_PUSHER_APP_KEY, 'CLUSTER:', import.meta.env.VITE_PUSHER_APP_CLUSTER);
 window.Echo = new Echo({
   broadcaster: 'pusher',
   key: import.meta.env.VITE_PUSHER_APP_KEY,
