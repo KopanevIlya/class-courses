1fa90f08a5dfce03b1aa7f21c8dcb24e37b4eef1
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-19 15:59:37 +0300

    video

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'name' => env('APP_NAME'),
+    'name' => env('APP_NAME', 'Class'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://localhost'),
